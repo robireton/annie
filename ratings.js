9a2f083226376f1c4796423f8515e008e466abf3
Rob Ireton <rob@ireton.org>
2025-06-06 15:51:05 -0400

    changing direction with ratings

--- ratings.js
+++ ratings.js
@@ -9,14 +9,9 @@ for (const track of tracks) {
   try {
     const t = track.properties()
     if (t.mediaKind !== 'song') continue
-    if (t.cloudStatus === 'no longer available') continue
+    // if (t.cloudStatus === 'no longer available') continue
     if (t.kind === 'PDF document') continue
-    // if (!t.enabled) continue
-
-    if (t.favorited && t.disliked) {
-      console.log(`☞ “${t.name}” by “${t.artist}” is both favorited and disliked ☜`)
-      continue
-    }
+    if (t.kind === 'iTunes LP') continue
 
     if (t.rating === 0) {
       console.log(`rating “${t.name}” by “${t.artist}” ${stars(60)}`)
@@ -24,27 +19,33 @@ for (const track of tracks) {
       t.rating = 60
     }
 
-    if (t.rating > 60 && !t.favorited) {
-      console.log(`favoriting “${t.name}” by “${t.artist}” (${stars(t.rating)})`)
-      track.favorited = true
-      continue
-    }
-
-    if (t.favorited && t.rating < 80) {
-      console.log(`rating “${t.name}” by “${t.artist}” ${stars(80)}`)
-      track.rating = 80
-      continue
-    }
-
-    if (t.rating < 60 && !t.disliked) {
+    if (t.rating < 40 && !t.disliked) {
       console.log(`disliking “${t.name}” by “${t.artist}” (${stars(t.rating)})`)
       track.disliked = true
       continue
     }
 
-    if (t.disliked && t.rating > 40) {
-      console.log(`rating “${t.name}” by “${t.artist}” ★★`)
-      track.rating = 40
+    if (t.disliked && t.rating > 20) {
+      console.log(`clearing disliked from “${t.name}” by “${t.artist}” (${stars(t.rating)})`)
+      track.disliked = false
+      continue
+    }
+
+    if (t.rating > 80 && !t.favorited) {
+      console.log(`favoriting “${t.name}” by “${t.artist}” (${stars(t.rating)})`)
+      track.favorited = true
+      continue
+    }
+
+    if (t.favorited && t.rating < 100) {
+      console.log(`clearing favorite from “${t.name}” by “${t.artist}” (${stars(t.rating)})`)
+      track.favorited = false
+      continue
+    }
+
+    if (t.favorited && t.disliked) {
+      console.log(`☞ “${t.name}” by “${t.artist}” is both favorited and disliked ☜`)
+      continue
     }
   } catch (err) {
     console.log(`caught error: “${err.message}” while processing “${track?.name()}” by “${track?.artist()}”`)
